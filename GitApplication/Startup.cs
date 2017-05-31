c7d08b24ea377c2e4676527b46952335eecf271a
vinodh <vinodh@vinodh.com>
2017-05-31 07:32:27 +0530

    initial project checkin

---
+++ GitApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitApplication.Startup))]
+namespace GitApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
